072191442aabc30dc5f9859d2d819efa73edbba8
Dhanesh A Nair <dhanesh.a.connect@gmail.com>
2026-05-30 23:10:53 +0530

    Added question

--- daily1.py
+++ daily1.py
@@ -1,3 +1,13 @@
+''' Question: Pizza Party
+Given an array of hours worked today per person,
+return the number of pizzas to order for a pizza party.
+
+Divide each person's hours worked by 3 to get their slice count.
+You can't eat a partial slice, so round each person's slice count up to the nearest whole number.
+Each person gets a minimum of two slices.
+Each pizza has 8 slices. Round the total number of pizzas up to the nearest whole pizza.
+'''
+
 import math 
 l=eval(input('Hours worked as a list: '))
 def get_pizzas_to_order(hours_worked): 
