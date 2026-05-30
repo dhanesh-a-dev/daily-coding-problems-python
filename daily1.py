33deefa0afe77386e61cc4f86dbfbe57d18bfd7f
dhanesh-a-dev <dhanesh.a.connect@gmail.com>
2026-05-30 23:13:19 +0530

    Updated daily1.py

--- daily1.py
+++ daily1.py
@@ -6,6 +6,8 @@ Divide each person's hours worked by 3 to get their slice count.
 You can't eat a partial slice, so round each person's slice count up to the nearest whole number.
 Each person gets a minimum of two slices.
 Each pizza has 8 slices. Round the total number of pizzas up to the nearest whole pizza.
+
+Question from FreeCodeCamp
 '''
 
 import math 
